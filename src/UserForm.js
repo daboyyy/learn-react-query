b0682daeac015f65fe85f81050475823a564a020
jiniusz <dev.jiniusz@gmail.com>
2021-09-16 12:52:02 +0700

    prevent optimistic updates failure

--- src/UserForm.js
+++ src/UserForm.js
@@ -10,14 +10,9 @@ const UserForm = ({ user, setIsEditing }) => {
     const queryClient = useQueryClient();
 
     const { isLoading, mutate } = useMutation(api.updateUser, {
-        onMutate: (updatedUser) => {
-            queryClient.setQueryData(['user'], user.id), updatedUser);
-
+        onSuccess: (data) => {
+            queryClient.setQueryData(['user'], user.id), data);
             setIsEditing(false);
-        },
-        onSuccess: () => {
-            // trigger the old data to be updated
-            queryClient.invalidateQueries(['user', user.id]);
         }
     });
 
